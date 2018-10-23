c71d5ca7fe9a9b08e968a0a0ee6c538cbd63ab47
Moon1 <moon0920@dreamwiz.com>
2018-10-23 13:44:58 +0900

    Initial commit

---
+++ app/src/main/java/com/mmj/www/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mmj.www.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onClicked (View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1234-1234"));
+        startActivity(intent);
+    }
+}
